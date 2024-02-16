82447c110e5ef33f982e691651ec726d4a1cfd91
Dominik Hel <domhel00@gmail.com>
2024-02-16 10:12:28 +0100

    Initial Commit

---
+++ GitHubCollaborationTestApp/GitHubCollaborationTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaborationTestAppApp.swift
+//  GitHubCollaborationTestApp
+//
+//  Created by Dominik Hel on 16.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCollaborationTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
