08f9e3ae9a45059b3b05be2d4fc3907ecdce64b8
Dominik Hel <domhel00@gmail.com>
2024-02-18 19:40:41 +0100

    Adding updateFile() function for adding new item and updating file.

--- GitHubCollaborationTestApp/ItemViewModel/ItemViewModel.swift
+++ GitHubCollaborationTestApp/ItemViewModel/ItemViewModel.swift
@@ -2,14 +2,24 @@ import Foundation
 
 class ItemViewModel: ObservableObject {
     @Published var items = [ItemModel]()
+    private let fileManager = ToDoFileManager()
 
     init() {
         do {
-            let fileManager = ToDoFileManager()
             let dataLoaded: [ItemModel] = try fileManager.loadDataFromFile(file: Constants.FileURL)
             self.items = dataLoaded
         } catch {
             items = []
+        }
+    }
+    
+    public func updateFile(with newItem: ItemModel) {
+        items.append(newItem)
+        do {
+            try fileManager.updateFile(for: Constants.FileURL, items: items)
+
+        } catch  {
+            fatalError()
         }
     }
 }
