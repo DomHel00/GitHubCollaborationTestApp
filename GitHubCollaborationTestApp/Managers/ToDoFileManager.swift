db068e452f15767d45518739960fd616be06ba97
Pavla Beránková <berankovap@protonmail.com>
2024-02-18 17:52:57 +0100

    Add func loadDateFromFile

--- GitHubCollaborationTestApp/Managers/ToDoFileManager.swift
+++ GitHubCollaborationTestApp/Managers/ToDoFileManager.swift
@@ -1,12 +1,19 @@
-//
-//  ToDoFileManager.swift
-//  GitHubCollaborationTestApp
-//
-//  Created by Dominik Hel on 16.02.2024.
-//
-
 import Foundation
 
 final class ToDoFileManager {
-    
+    public func loadDateFromFile<T: Codable>(file url: URL) throws -> T {
+            guard let data = try? Data(contentsOf: url) else {
+                throw JSONFileManagerError.invalidData
+            }
+
+            guard let decodedData = try? JSONDecoder().decode(T.self, from: data) else {
+                throw JSONFileManagerError.decoderError
+            }
+            return decodedData
+        }
+}
+
+enum JSONFileManagerError: Error {
+    case invalidData
+    case decoderError
 }
