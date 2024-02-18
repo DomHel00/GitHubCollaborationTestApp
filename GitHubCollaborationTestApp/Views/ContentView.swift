5b833d4149b42662cf6e1d6fc00ae4b1a6815687
Dominik Hel <domhel00@gmail.com>
2024-02-18 19:22:58 +0100

    Connecting ToDoListView and AddItemView.

--- GitHubCollaborationTestApp/Views/ContentView.swift
+++ GitHubCollaborationTestApp/Views/ContentView.swift
@@ -2,35 +2,41 @@ import SwiftUI
 
 struct ToDoListView: View {
     @StateObject var model = ItemViewModel()
-
+    
     var body: some View {
         NavigationStack {
-            if !model.items.isEmpty {
-                List {
-                    ForEach(model.items, id: \.title) { item in
-                        Text(item.title)
+            Group {
+                if !model.items.isEmpty {
+                    List {
+                        ForEach(model.items, id: \.title) { item in
+                            Text(item.title)
+                        }
+                    }
+                } else {
+                    VStack(spacing: 50) {
+                        Image(systemName: "note.text.badge.plus")
+                            .resizable()
+                            .frame(width: 120, height: 100)
+                            .opacity(0.3)
+                        Text("Nic tu ještě nemáš")
+                            .font(.largeTitle).opacity(0.3)
                     }
                 }
-            } else {
-                VStack(spacing: 50) {
-                    Image(systemName: "note.text.badge.plus")
-                        .resizable()
-                        .frame(width: 120, height: 100)
-                        .opacity(0.3)
-                    Text("Nic tu ještě nemáš")
-                        .font(.largeTitle).opacity(0.3)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink {
+                        AddItemView { newItem in
+                            
+                        }
+                    } label: {
+                        Label("Add new item", systemImage: "plus")
+                    }
+                    
                 }
             }
         }
-        .toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button {
-                    // function - add new item
-                } label: {
-                    Image(systemName: "plus")
-                }
-            }
-        }
+        
     }
 }
 
