91fc3e6976416ad2284b741118197a6a48ac9b2f
Dominik Hel <domhel00@gmail.com>
2024-02-19 11:10:36 +0100

    Creating a new enum ToDoItemPriority and adding new priority variable to the ToDoItem.

--- GitHubCollaborationTestApp/Models/ToDoItem.swift
+++ GitHubCollaborationTestApp/Models/ToDoItem.swift
@@ -1,15 +1,29 @@
 import Foundation
 
+enum ToDoItemPriority: Codable, CaseIterable {
+    case low, normal, high
+    
+    var title: String {
+        switch self {
+        case .low: return "Low"
+        case .normal: return "Normal"
+        case .high: return "High"
+        }
+    }
+}
+
 struct ToDoItem: Codable {
     var title: String
     let creationDate: Date
     var finishDate: Date
     var isComplete: Bool
+    var priority: ToDoItemPriority
     
-    init(title: String, finishDate: Date) {
+    init(title: String, finishDate: Date, priority: ToDoItemPriority) {
         self.title = title
         self.creationDate = .now
         self.finishDate = finishDate
         self.isComplete = false
+        self.priority = priority
     }
 }
