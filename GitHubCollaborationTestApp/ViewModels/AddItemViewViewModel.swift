2552aabad9915a6a69d8ceb69534ce78d27e99db
Dominik Hel <domhel00@gmail.com>
2024-02-19 11:11:44 +0100

    Updating AddItemViewViewModel for using new model with priority.

--- GitHubCollaborationTestApp/ViewModels/AddItemViewViewModel.swift
+++ GitHubCollaborationTestApp/ViewModels/AddItemViewViewModel.swift
@@ -10,7 +10,8 @@ import Foundation
 final class AddItemViewViewModel: ObservableObject {
     @Published var title: String = ""
     @Published var finishDate: Date = .now
-    
+    @Published var priority: ToDoItemPriority = .normal
+
     var isValid: Bool {
         let trimmed = title.trimmingCharacters(in: .whitespacesAndNewlines)
 
@@ -22,7 +23,7 @@ final class AddItemViewViewModel: ObservableObject {
     
     public func createNewItem() -> ToDoItem {
         let trimmedTitle = title.trimmingCharacters(in: .whitespacesAndNewlines)
-        let newItem = ToDoItem(title: trimmedTitle, finishDate: finishDate)
+        let newItem = ToDoItem(title: trimmedTitle, finishDate: finishDate, priority: priority)
         return newItem
     }
 }
